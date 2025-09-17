2e3c4bb2a16e7a2721d78def41de2ced0f7f9c7f
Mikita Dzeviatau <9mikita@gmail.com>
2025-09-17 20:25:45 +0200

    Add problem 112

--- src/_112_Path_Sum.java
+++ src/_112_Path_Sum.java
@@ -1,11 +1,43 @@
+import com.sun.source.tree.Tree;
+
 public class _112_Path_Sum {
     public static void main(String[] args) {
 
     }
 
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
 
 
-    private static boolean hasPathSum(TreeNode root, int targetSum) {
+        private static boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) return false;
 
+            int subSum = targetSum - root.val;
+            boolean left = false, right = false;
+
+            if (subSum == 0 && root.left == null && root.right == null) {
+                return true;
+            }
+
+            if (root.left != null) left = hasPathSum(root.left, subSum);
+            if (root.right != null) right = hasPathSum(root.right, subSum);
+
+            return left || right;
+        }
     }
 }
