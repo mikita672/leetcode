b6f506d1471745b0c8922185b735a8c1579d00dc
Mikita Dzeviatau <9mikita@gmail.com>
2025-09-21 16:44:13 +0200

    Add problem 144

---
+++ src/_144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _144_Binary_Tree_Preorder_Traversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println("Recursive: " + preorderTraversal(root));
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    private static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        performPreorderDFS(root, result);
+        return result;
+    }
+
+    private static void performPreorderDFS(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        result.add(node.val);
+        performPreorderDFS(node.left, result);
+        performPreorderDFS(node.right, result);
+    }
+}
