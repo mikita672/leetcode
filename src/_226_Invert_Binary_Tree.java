c78999f69e8395488125df0c062003ee80a6fcf6
Mikita Dzeviatau <9mikita@gmail.com>
2025-09-28 16:39:26 +0200

    Add problem 226

---
+++ src/_226_Invert_Binary_Tree.java
@@ -0,0 +1,37 @@
+public class _226_Invert_Binary_Tree {
+    public static void main(String[] args) {
+
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    private static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
