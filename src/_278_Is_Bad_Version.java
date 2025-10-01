9b9b4605befd4f897aad5552f0f13706d69d3663
Mikita Dzeviatau <9mikita@gmail.com>
2025-10-01 16:45:47 +0200

    Add problem 278

---
+++ src/_278_Is_Bad_Version.java
@@ -0,0 +1,23 @@
+public class _278_Is_Bad_Version {
+    public static void main(String[] args) {
+
+    }
+
+    public int firstBadVersion(int n) {
+        int l = 0, h = n;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (isBadVersion(mid)) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    private boolean isBadVersion(int mid) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isBadVersion'");
+    }
+}
