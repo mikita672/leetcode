45f10903a70473d2ad59f5e045ff7b244117a7ea
Mikita Dzeviatau <9mikita@gmail.com>
2025-10-05 17:12:59 +0200

    Add problem 350

---
+++ src/_350_Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class _350_Intersection_of_Two_Arrays_II {
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 2, 1 };
+        int[] nums2 = { 2, 2 };
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+    }
+
+    private static int[] intersect(int[] nums1, int[] nums2) {
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0, j = 0, k = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        while (i < l1 && j < l2) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                nums1[k++] = nums1[i++];
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
