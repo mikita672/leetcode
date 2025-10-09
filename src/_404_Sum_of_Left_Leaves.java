46d521b80ea6bfd0954fae2891c9f4b0e494d138
Mikita Dzeviatau <9mikita@gmail.com>
2025-10-09 23:03:45 +0200

    Add problem 404

---
+++ src/_404_Sum_of_Left_Leaves.java
@@ -0,0 +1,42 @@
+public class _404_Sum_of_Left_Leaves {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        }
+
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+}
