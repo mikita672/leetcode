037a3ff35467237c9c7b736de1363c1fe8a0a3ae
Mikita Dzeviatau <9mikita@gmail.com>
2025-10-12 19:06:26 +0200

    Add problem 415

---
+++ src/_415_Add_String.java
@@ -0,0 +1,24 @@
+public class _415_Add_String {
+    public static void main(String[] args) {
+        System.out.println(addStrings("11", "123"));
+        System.out.println(addStrings("456", "77"));
+    }
+
+    public static String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int d1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int d2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            int sum = d1 + d2 + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+}
