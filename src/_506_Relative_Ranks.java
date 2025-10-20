878c934bdd808cd64dd5f90dc4f7c9e25c9fa9b1
Mikita Dzeviatau <9mikita@gmail.com>
2025-10-20 15:20:04 +0200

    Add problem 506

---
+++ src/_506_Relative_Ranks.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class _506_Relative_Ranks {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findRelativeRanks(new int[] { 5, 4, 3, 2, 1 })));
+        System.out.println(Arrays.toString(findRelativeRanks(new int[] { 10, 3, 8, 9, 4 })));
+    }
+
+    public static String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+
+        int[][] sortedPairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            sortedPairs[i] = new int[] { i, score[i] };
+        }
+
+        Arrays.sort(sortedPairs, (x, y) -> (y[1] - x[1]));
+
+        String[] ans = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                ans[sortedPairs[i][0]] = "Gold Medal";
+            } else if (i == 1) {
+                ans[sortedPairs[i][0]] = "Silver Medal";
+            } else if (i == 2) {
+                ans[sortedPairs[i][0]] = "Bronze Medal";
+            } else {
+                ans[sortedPairs[i][0]] = String.valueOf(i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
